f71e18e850965576ef500ac1d042cd3ab9292c99
Muhammad Orabi <morabi@quadrantme.com>
2018-06-03 13:43:33 +0200

    fix issue

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/webhock', 'ChatBotController@get');
+Route::post('/webhock', 'ChatBotController@post');
