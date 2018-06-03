a682b79f1ce36c030d0b30cf9d37815eb916095a
Muhammad Orabi <morabi@quadrantme.com>
2018-06-03 14:42:02 +0200

    fix issue

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -22,21 +22,21 @@ class ChatBotController extends Controller
 
     public function post(Request $request)
     {
-        $messaging_events = $request->all()['entry'][0]['messaging'];
-        foreach ($messaging_events as $event) {
-            $sender = $event['sender']['id'];
-            if ($event['message'] && $event['message']['text']) {
-                $data = ['text' => $event['message']['text']];
-                $client = new \GuzzleHttp\Client();
-                $res = $client->request('POST', 'https://graph.facebook.com/v2.6/me/messages', [
-                    'query' => ['access_token' => env('CHATPOT_PAGE_ACCESS_TOKEN')],
-                    'json' => [
-                        'recipient' => ['id' => $sender],
-                        'message' => $data
-                    ]
-                ]);
-            }
-        }
+        // $messaging_events = $request->all()['entry'][0]['messaging'];
+        // foreach ($messaging_events as $event) {
+        //     $sender = $event['sender']['id'];
+        //     if ($event['message'] && $event['message']['text']) {
+        //         $data = ['text' => $event['message']['text']];
+        //         $client = new \GuzzleHttp\Client();
+        //         $res = $client->request('POST', 'https://graph.facebook.com/v2.6/me/messages', [
+        //             'query' => ['access_token' => env('CHATPOT_PAGE_ACCESS_TOKEN')],
+        //             'json' => [
+        //                 'recipient' => ['id' => $sender],
+        //                 'message' => $data
+        //             ]
+        //         ]);
+        //     }
+        // }
         return response(200);
     }
 }
