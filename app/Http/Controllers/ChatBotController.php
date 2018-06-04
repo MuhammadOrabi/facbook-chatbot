866ed03c380c5e8e6278af92b57ab773208c7ef0
Muhammad Orabi <morabi@quadrantme.com>
2018-06-04 11:42:51 +0200

    debugging

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -28,7 +28,6 @@ class ChatBotController extends Controller
             $sender = $event['sender']['id'];
             if ($event['message'] && $event['message']['text']) {
                 $data = ['text' => $event['message']['text']];
-                file_put_contents('postLog.txt', $data);            
                 $client = new \GuzzleHttp\Client();
                 $res = $client->request('POST', 'https://graph.facebook.com/v2.6/me/messages', [
                     'query' => ['access_token' => env('CHATPOT_PAGE_ACCESS_TOKEN')],
@@ -38,6 +37,8 @@ class ChatBotController extends Controller
                         'message' => $data
                     ]
                 ]);
+            } else {
+                file_put_contents('postLog.txt', '$data');                            
             }
         }
         return response(200);
