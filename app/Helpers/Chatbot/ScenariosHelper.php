4c5e1aabe2907a5292188ad11d6fdf6812ad341d
Muhammad Orabi <morabi@quadrantme.com>
2018-06-05 14:16:34 +0200

    debugging

--- app/Helpers/Chatbot/ScenariosHelper.php
+++ app/Helpers/Chatbot/ScenariosHelper.php
@@ -8,13 +8,10 @@ class ScenariosHelper
     {
         if (strpos($message, 'hello') !== false) {
             return ReplyHelper::text('Hey, How are you doing?');
-        } else if (strpos($message, 'webs') !== false) {
-            return ReplyHelper::buttons('Surfe the web', [
-                ['type' => 'web_url', 'url' => 'https://google.com', 'title' => 'Google'],
-                ['type' => 'web_url', 'url' => 'https://facebook.com', 'title' => 'Facebook'],
-                ['type' => 'web_url', 'url' => 'https://youtube.com', 'title' => 'YouTube'],
-                ['type' => 'web_url', 'url' => 'https://twitter.com', 'title' => 'Twitter'],
-            ]);
+        } else if (strpos($message, 'web') !== false) {
+            return ReplyHelper::buttons();
+        } else {
+            return ReplyHelper::quickReply();
         }
     }
 }
