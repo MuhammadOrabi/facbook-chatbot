6d72e974204a0c3d87e20305055810a6ed35cc13
Muhammad Orabi <morabi@quadrantme.com>
2018-06-05 15:19:35 +0200

    debugging

--- app/Helpers/Chatbot/ReplyHelper.php
+++ app/Helpers/Chatbot/ReplyHelper.php
@@ -5,9 +5,9 @@ namespace App\Helpers\Chatbot;
 class ReplyHelper
 {
 
-    public static function handle($type, $title, $data)
+    public static function handle($type, $data)
     {
-        return self::$type($title, $data);
+        return self::$type($data);
     }
 
     public static function text($message)
@@ -17,51 +17,36 @@ class ReplyHelper
         ];
     }
 
-    public static function generic($title, $data)
+    public static function genericElements($data)
     {
+        $elements = [];
+        $elements[] = $data->map(function ($item) {
+            $elm = [
+                "title" => $item['title'], 
+                "item_url" => 'https://www.qarenhom.com/en/' . $item['title'], 
+                "image_url" => "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",         
+            ];
+            $elm['buttons'][] = $item['sections']->map(function ($section, $key, $item) {
+                return [
+                    "type" => "web_url", 
+                    "url" => 'https://www.qarenhom.com/en/' . $item . '/' . $section['title'], 
+                    "title" => $section['title']
+                ];
+            });
+            return $elm;
+        }); 
+        return $elements; 
+    }
+
+    public static function generic($data)
+    {
+        $elements = self::genericElements($data);
           return [
             "attachment" => [
                 "type" => "template", 
                 "payload" => [
                     "template_type" => "generic",
-                    "elements" => [
-                        [
-                            "title" => $title, 
-                            "item_url" => "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/", 
-                            "image_url" => "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg", 
-                            "subtitle" => "Migrate your symfony application from Cpanel to Cloud.", 
-                            "buttons" => [
-                                [
-                                    "type" => "web_url", 
-                                    "url" => "www.cloudways.com", 
-                                    "title" => "View Website"
-                                ], 
-                                [
-                                    "type" => "postback", 
-                                    "title" => "Start Chatting", 
-                                    "payload" => "get started"
-                                ]
-                            ]
-                        ],
-                        [
-                            "title" => $title, 
-                            "item_url" => "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/", 
-                            "image_url" => "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg", 
-                            "subtitle" => "Migrate your symfony application from Cpanel to Cloud.", 
-                            "buttons" => [
-                                [
-                                    "type" => "web_url", 
-                                    "url" => "www.cloudways.com", 
-                                    "title" => "View Website"
-                                ], 
-                                [
-                                    "type" => "postback", 
-                                    "title" => "Start Chatting", 
-                                    "payload" => "get started"
-                                ]
-                            ]
-                        ]
-                    ]
+                    "elements" => $elements
                 ]
             ]
         ];
--- app/Helpers/Chatbot/ScenariosHelper.php
+++ app/Helpers/Chatbot/ScenariosHelper.php
@@ -8,14 +8,46 @@ class ScenariosHelper
     {
         if (strpos($message, 'hello') !== false) {
             // return ReplyHelper::text('Hey, wanna look for new deals?');
-            $departments = collect([
-                ['id' => 1, 'title' => 'travel'],
-                ['id' => 2, 'title' => 'banking'],
-                ['id' => 3, 'title' => 'insurance'],
-                ['id' => 4, 'title' => 'education'],
+            $data = collect([
+                [
+                    'id' => 1, 'title' => 'banking', 
+                    'sections' => [
+                        ['id' => 1, 'title' => 'credit-card'],
+                        ['id' => 2, 'title' => 'personal-loan'],
+                        ['id' => 3, 'title' => 'car-loan'],
+                        ['id' => 4, 'title' => 'mortage-finance'],
+                        ['id' => 5, 'title' => 'account'],
+                        ['id' => 6, 'title' => 'deposite'],
+                    ]
+                ],
+                [
+                    'id' => 2, 'title' => 'insurance', 
+                    'sections' => [['id' => 1, 'title' => 'car-insurance']]
+                ],
+                [
+                    'id' => 3, 'title' => 'travel',
+                    'sections' => [
+                        ['id' => 1, 'title' => 'domestic'],
+                        ['id' => 2, 'title' => 'international'],
+                        ['id' => 3, 'title' => 'honeymoon-domestic'],
+                        ['id' => 4, 'title' => 'hajj'],
+                        ['id' => 5, 'title' => 'umrah'],
+                        ['id' => 6, 'title' => 'visa'],
+                    ]
+                ],
+                [
+                    'id' => 4, 'title' => 'education', 
+                    'sections' => [
+                        ['id' => 1, 'title' => 'development'],
+                        ['id' => 2, 'title' => 'language'],
+                        ['id' => 3, 'title' => 'kids-language'],
+                        ['id' => 4, 'title' => 'kids-development'],
+                        ['id' => 5, 'title' => 'study-abroad'],
+                    ]
+                ],
             ]);
 
-            return ReplyHelper::handle('generic', 'Choose one of our departments', $departments);
+            return ReplyHelper::handle('generic', $data);
         } else if (strpos($message, 'web') !== false) {
             return ReplyHelper::generic('This is a title', []);
         } else {
