b17af0385621fe34d6d59933b484f6683b189113
Muhammad Orabi <morabi@quadrantme.com>
2018-06-05 10:24:23 +0200

    debugging

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -22,7 +22,6 @@ class ChatBotController extends Controller
 
     public function post(Request $request)
     {
-        echo "here";
         $messaging_events = $request->all()['entry'][0]['messaging'];
         foreach ($messaging_events as $event) {
             $sender = $event['sender']['id'];
