d07a86f604e2fa25fb2806e207eea8ca2132168d
Muhammad Orabi <morabi@quadrantme.com>
2018-06-06 10:54:32 +0200

    debugging

--- app/Helpers/Chatbot/ReplyHelper.php
+++ app/Helpers/Chatbot/ReplyHelper.php
@@ -38,7 +38,7 @@ class ReplyHelper
         return $elements; 
     }
 
-    public static function generic($data)
+    public static function generics($data)
     {
         $elements = self::genericElements($data);
           return [
@@ -47,6 +47,38 @@ class ReplyHelper
                 "payload" => [
                     "template_type" => "generic",
                     "elements" => $elements
+                ]
+            ]
+        ];
+    }
+
+    public static function generic($data)
+    {
+        return [
+            "attachment" => [
+                "type" => "template", 
+                "payload" => [
+                    "template_type" => "generic", 
+                    "elements" => [
+                        [
+                            "title" => "Migrate your symfony application", 
+                            "item_url" => "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/", 
+                            "image_url" => "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg", 
+                            "subtitle" => "Migrate your symfony application from Cpanel to Cloud.", 
+                            "buttons" => [
+                                [
+                                    "type" => "web_url", 
+                                    "url" => "www.cloudways.com", 
+                                    "title" => "View Website"
+                                ], 
+                                [
+                                    "type" => "postback", 
+                                    "title" => "Start Chatting", 
+                                    "payload" => "get started"
+                                ]
+                            ]
+                        ]
+                    ]
                 ]
             ]
         ];
