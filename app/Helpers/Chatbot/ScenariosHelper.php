8554f7be7d891935c599dd804ecffc6c53033c47
Muhammad Orabi <morabi@quadrantme.com>
2018-06-06 12:38:43 +0200

    debugging

--- app/Helpers/Chatbot/ScenariosHelper.php
+++ app/Helpers/Chatbot/ScenariosHelper.php
@@ -13,11 +13,11 @@ class ScenariosHelper
                     'id' => 1, 'title' => 'banking', 
                     'sections' => [
                         ['id' => 1, 'title' => 'credit-card'],
-                        ['id' => 2, 'title' => 'personal-loan'],
-                        ['id' => 3, 'title' => 'car-loan'],
-                        ['id' => 4, 'title' => 'mortage-finance'],
-                        ['id' => 5, 'title' => 'account'],
-                        ['id' => 6, 'title' => 'deposite'],
+                        // ['id' => 2, 'title' => 'personal-loan'],
+                        // ['id' => 3, 'title' => 'car-loan'],
+                        // ['id' => 4, 'title' => 'mortage-finance'],
+                        // ['id' => 5, 'title' => 'account'],
+                        // ['id' => 6, 'title' => 'deposite'],
                     ]
                 ],
                 [
@@ -28,21 +28,21 @@ class ScenariosHelper
                     'id' => 3, 'title' => 'travel',
                     'sections' => [
                         ['id' => 1, 'title' => 'domestic'],
-                        ['id' => 2, 'title' => 'international'],
-                        ['id' => 3, 'title' => 'honeymoon-domestic'],
-                        ['id' => 4, 'title' => 'hajj'],
-                        ['id' => 5, 'title' => 'umrah'],
-                        ['id' => 6, 'title' => 'visa'],
+                        // ['id' => 2, 'title' => 'international'],
+                        // ['id' => 3, 'title' => 'honeymoon-domestic'],
+                        // ['id' => 4, 'title' => 'hajj'],
+                        // ['id' => 5, 'title' => 'umrah'],
+                        // ['id' => 6, 'title' => 'visa'],
                     ]
                 ],
                 [
                     'id' => 4, 'title' => 'education', 
                     'sections' => [
                         ['id' => 1, 'title' => 'development'],
-                        ['id' => 2, 'title' => 'language'],
-                        ['id' => 3, 'title' => 'kids-language'],
-                        ['id' => 4, 'title' => 'kids-development'],
-                        ['id' => 5, 'title' => 'study-abroad'],
+                        // ['id' => 2, 'title' => 'language'],
+                        // ['id' => 3, 'title' => 'kids-language'],
+                        // ['id' => 4, 'title' => 'kids-development'],
+                        // ['id' => 5, 'title' => 'study-abroad'],
                     ]
                 ],
             ];
