b46fd04252886c2182bf09871028a5f5ce540692
Muhammad Orabi <morabi@quadrantme.com>
2018-06-06 14:17:15 +0200

    debugging

--- app/Helpers/Chatbot/ReplyHelper.php
+++ app/Helpers/Chatbot/ReplyHelper.php
@@ -20,7 +20,7 @@ class ReplyHelper
     public static function genericElements($data)
     {
         $elements = [];
-        foreach ($data as $item) {
+        foreach ($data->toArray() as $item) {
             $elements[] = [
                 "title" => $item['title'], 
                 "item_url" => 'https://www.qarenhom.com/en/' . $item['title'], 
@@ -57,42 +57,10 @@ class ReplyHelper
         ];
     }
 
-    public static function generics($data)
-    {
-        return [
-            "attachment" => [
-                "type" => "template", 
-                "payload" => [
-                    "template_type" => "generic", 
-                    "elements" => [
-                        [
-                            "title" => "Migrate your symfony application", 
-                            "item_url" => "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/", 
-                            "image_url" => "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg", 
-                            "subtitle" => "Migrate your symfony application from Cpanel to Cloud.", 
-                            "buttons" => [
-                                [
-                                    "type" => "web_url", 
-                                    "url" => "www.cloudways.com", 
-                                    "title" => "View Website"
-                                ], 
-                                [
-                                    "type" => "postback", 
-                                    "title" => "Start Chatting", 
-                                    "payload" => "get started"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ];
-    }
-
     public static function listElements($data)
     {
         $elements = [];
-        foreach ($data as $item) {
+        foreach ($data->toArray() as $item) {
             $elements[] = [
             "title"=>"Migrate symfony from cpanel",
             "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
--- app/Helpers/Chatbot/ScenariosHelper.php
+++ app/Helpers/Chatbot/ScenariosHelper.php
@@ -7,42 +7,42 @@ class ScenariosHelper
     public static function handle($message)
     {
         $data = [
-                    [
+                    collect([
                         'id' => 1, 'title' => 'banking', 
-                        'sections' => [
+                        'sections' => collect([
                             ['id' => 1, 'title' => 'credit-card'],
                             ['id' => 2, 'title' => 'personal-loan'],
                             ['id' => 3, 'title' => 'car-loan'],
                             ['id' => 4, 'title' => 'mortage-finance'],
                             ['id' => 5, 'title' => 'account'],
                             ['id' => 6, 'title' => 'deposite'],
-                        ]
-                    ],
-                    [
+                        ])
+                    ]),
+                    collect([
                         'id' => 2, 'title' => 'insurance', 
-                        'sections' => [['id' => 1, 'title' => 'car-insurance']]
-                    ],
-                    [
+                        'sections' => collect([['id' => 1, 'title' => 'car-insurance']])
+                    ]),
+                    collect([
                         'id' => 3, 'title' => 'travel',
-                        'sections' => [
+                        'sections' => collect([
                             ['id' => 1, 'title' => 'domestic'],
                             ['id' => 2, 'title' => 'international'],
                             ['id' => 3, 'title' => 'honeymoon-domestic'],
                             ['id' => 4, 'title' => 'hajj'],
                             ['id' => 5, 'title' => 'umrah'],
                             ['id' => 6, 'title' => 'visa'],
-                        ]
-                    ],
-                    [
+                        ])
+                    ]),
+                    collect([
                         'id' => 4, 'title' => 'education', 
-                        'sections' => [
+                        'sections' => collect([
                             ['id' => 1, 'title' => 'development'],
                             ['id' => 2, 'title' => 'language'],
                             ['id' => 3, 'title' => 'kids-language'],
                             ['id' => 4, 'title' => 'kids-development'],
                             ['id' => 5, 'title' => 'study-abroad'],
-                        ]
-                    ],
+                        ])
+                    ]),
                 ];
         if (strpos($message, 'list') !== false) {
             return ReplyHelper::handle('list', $data);
